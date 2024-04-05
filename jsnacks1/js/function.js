83cd17b275696fd7c72ededb08f189cd5a2ffd7f
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-05 11:45:47 +0200

    Feat: jsnacks 1

---
+++ jsnacks1/js/function.js
@@ -0,0 +1,12 @@
+// genera array
+
+/**
+ * Description
+ * @param{number} num 
+ * @param{number}  
+ * @returns {any}
+ */
+function generaNbr(min, max) {
+    let numRandom = Math.floor(Math.random() * max - min + 1) + min;
+    return numRandom
+}
---
+++ jsnacks1/js/script.js
@@ -0,0 +1,10 @@
+
+// Numero inserito dall'utente tramite prompt
+
+const numUtente = parseInt(prompt("inserisci un numero N"))
+
+let nArray = numUtente;
+
+for(let i = 0; i<= nArray.length; i++) {
+    let numArray = generaNbr()
+}
