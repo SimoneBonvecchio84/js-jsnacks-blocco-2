5eb78a7ec12a579d029d2a3db958f1292285c9d1
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-09 22:48:56 +0200

    Feat: add commit to script and function

--- jsnacks4/js/function.js
+++ jsnacks4/js/function.js
@@ -7,15 +7,19 @@
  * @returns {number} number
  */
 function trovaIndice(array, elemArray, indexElem) {
-    
+    // tramite ciclo for controllo se il parametro è elemArray
+    // è presente all'interno dell'array
     for(let i = 0; i < array.length; i++) {
-        indexElem = -1;
         if(elemArray === array[i]){
+            // valore che mi deve restituire la funzione è l'indice
             return i;
         } 
         
     }
-    
+    // se index elem è diverso da -1 restituisci il numero di indice
+    // dell elemento corrispondente
+    // altrimenti mi restituisci il valore di default che ho depositato
+    // nella variabile indexElem nello script
     if(indexElem !== -1) {
         return indexElem;
     } else {
--- jsnacks4/js/script.js
+++ jsnacks4/js/script.js
@@ -1,21 +1,13 @@
+// INPUT
+// array 
 const array = ["pippo", "franco", "ciccio", "ada"];
-
+// elemeto da inserire tramite prompt
 let elemToCheck = prompt("inserisci un nome")
-
+// variabile nella quale deposito il valore -1 di default
 let indexElem = -1;
 
 
-// for (let i = 0; i < array.length; i++){
-//     if(elemToCheck === array[i]){
-//         indexElem = i;
-//     } 
-// }
-//  if (indexElem !== -1) {
-//     console.log(indexElem)
-//  } else {
-//     console.log(indexElem);
-//  }
-
+// invoco la funzione che deposito all' interno della variabile risultato
 let risultato = trovaIndice(array, elemToCheck, indexElem);
 
 console.log(risultato);
